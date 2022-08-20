7be46c6ed5feac3435fe35638734d013c1ca2314
Краснов Александр <a.a.krasnov@tinkoff.ru>
2022-08-20 17:45:30 +0300

    fix: codestyle

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -3,7 +3,7 @@ package ru.job4j;
 public class Multiple {
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
-            System.out.printf("%d * %d = %d%n", 1, i,1*i);
+            System.out.printf("%d * %d = %d%n", 1, i, 1 * i);
         }
     }
 }
